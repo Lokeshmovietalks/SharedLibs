2f50b1eec5a711a6cf93b34d56068986fc832dae
Lokeshmovietalks <159557816+Lokeshmovietalks@users.noreply.github.com>
2024-10-16 13:15:25 +0530

    Update tomcat.groovy

--- vars/tomcat.groovy
+++ vars/tomcat.groovy
@@ -1,6 +1,6 @@
 def call(credID,filename,userID,PrivateIP){
   sshagent(["${credID}"]){
-	sh "scp -o StrictHostKeyChecking=no /target/${filename}.war ${userID}@${PrivateIP}:/opt/tomcat9/webapps"
+	sh "scp -o StrictHostKeyChecking=no /target/${filename}.war ${userID}@${PrivateIP}:/opt/tomcat9/webapps/"
 	sh "ssh ${userID}@${PrivateIP} /opt/tomcat9/bin/shutdown.sh"
 	sh "ssh ${userID}@${PrivateIP} /opt/tomcat9/bin/startup.sh"
   }
