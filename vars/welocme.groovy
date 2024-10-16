45a9d561a429c17e411659ddc7b08478fe4eb45e
Lokeshmovietalks <159557816+Lokeshmovietalks@users.noreply.github.com>
2024-10-16 11:22:26 +0530

    Update welocme.groovy

--- vars/welocme.groovy
+++ vars/welocme.groovy
@@ -1,3 +1,9 @@
 def call(name){
   echo "Hi ${name} Welcome to Shared Library"
 }
+def add(x,y){
+  echo "sum of ${x} and ${y} is ${x+y}"
+}
+def multiply(x,y){
+  echo "product of ${x} and ${y} is ${x*y}"
+}
