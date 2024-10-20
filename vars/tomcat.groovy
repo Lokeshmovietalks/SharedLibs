271c56aa7f6f25faaa2ea09b4fe46bc7818c3029
Lokeshmovietalks <159557816+Lokeshmovietalks@users.noreply.github.com>
2024-10-20 13:39:11 +0530

    Update tomcat.groovy

--- vars/tomcat.groovy
+++ vars/tomcat.groovy
@@ -5,3 +5,10 @@ def call(credID,filename,userID,PrivateIP){
 	sh "ssh ${userID}@${PrivateIP} /opt/tomcat9/bin/startup.sh"
   }
 }
+def nexus(artifactId,creds){
+	script{
+                    def pomfile=readMavenPom file: 'pom.xml'
+                    version=pomfile.version
+                    nexusArtifactUploader artifacts: [[artifactId: "${"artifactId"}", classifier: '', file: "target/myweb-${version}.war", type: 'war']], credentialsId: "${"creds"}", groupId: 'in.javahome', nexusUrl: '43.205.236.218:8081', nexusVersion: 'nexus3', protocol: 'http', repository: 'Myweb', version: "${version}"
+                }
+}
